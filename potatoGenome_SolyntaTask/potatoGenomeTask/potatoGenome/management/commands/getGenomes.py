187216acda6ceea422933f4e5bf5ebb44ad5ec1f
Samhita <samhitapn.96@gmail.com>
2023-06-04 22:48:57 +0530

    Update getGenomes.py

--- potatoGenome_SolyntaTask/potatoGenomeTask/potatoGenome/management/commands/getGenomes.py
+++ potatoGenome_SolyntaTask/potatoGenomeTask/potatoGenome/management/commands/getGenomes.py
@@ -14,29 +14,13 @@ from potatoGenome.getPotatoGenomes import getGenomeData
 
 from datetime import datetime
 
-#import logging
-
-#logger = logging.getLogger(__name__)
-
 class Command(BaseCommand):
     help = "Fetches the potato whole genomes and their (meta)data posted after 2018"
         
     def handle(self, *args, **kwargs):
-        
-        #start = datetime.now()
-        #logger.info("\n################# RUN getGenomes Command #################\n")
-        #logger.info(f"The Run Started at : {start}")
-        
+                
         getGenomeData()
-        
-        #end = datetime.now()
-        #logger.info(f"The Run Ended at : {end}")
-        
-        #rt = end - start
-        #run_time = rt.total_seconds()
-        
-        #logger.info(f"The Run took : {run_time} seconds to extract the data")
-        #logger.info("\n#################\n")
+
 
             
             
